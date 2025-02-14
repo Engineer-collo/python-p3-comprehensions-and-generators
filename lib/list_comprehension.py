6202a95e7986701a37673dddafe5966313b2bc72
Engineer-Collo <collinslikhomba@gmail.com>
2025-02-14 13:18:01 +0300

    compression list

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,22 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    if len(num_list) == 0:
+        return []
+    
+    return [num for num in num_list if num % 2 == 0]
+
+return_evens([0, 1, 3, 5, 7, 8, 9])
+
+
+
 
 def make_exclamation(sentence_list):
-    pass
+    my_char = "!"
+    #if len(sentence_list) == 0:
+        # return
+    return [sentence + my_char for sentence in  sentence_list  if sentence_list[-1] != my_char ]
+        
+        
+
+make_exclamation(["Hello", "I'm doing great", "Python is fun"])
